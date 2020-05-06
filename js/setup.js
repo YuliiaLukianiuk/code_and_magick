c0cae82f2efd6068cf8f2a9801f3b35ae84c6021
Yuliia.Lukianiuk <lukianiky@gmail.com>
2020-05-06 23:35:37 +0300

    open_close popap

--- js/setup.js
+++ js/setup.js
@@ -1,15 +1,73 @@
 'use strict';
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = document.querySelector('.setup-close');
 var similarListElement = document.querySelector('.setup-similar-list');
 var userNameInput = document.querySelector('.setup-user-name');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector(".setup-similar-item");
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SERNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-
 var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+setupOpen.addEventListener('click', function() {
+  userDialog.classList.remove('hidden');
+});
+setupClose.addEventListener('click', function() {
+  userDialog.classList.add('hidden');
+});
+
+var onPopupEscPress = function(evt) {
+  if (evt.keyCode=== ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function() {
+  userDialog.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function() {
+  userDialog.classList.add('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function() {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function(evt) {
+  if (evt.keyCode=== ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+setupClose.addEventListener('click', function() {
+  closePopup();
+});
+setupClose.addEventListener('keydown', function(evt) {
+  if(evt.keyCode=== ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+userNameInput.addEventListener('invalid', function (evt) {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  }else {
+    //cброс поля, если значение стало корректно
+    userNameInput.setCustomValidity('');
+  }
+  });
+
+
 var wizards = [
   {
     name:WIZARD_NAMES[Math.floor(Math.random() * WIZARD_NAMES.length)] + ' ' + WIZARD_SERNAME[Math.floor(Math.random() * WIZARD_SERNAME.length)],
