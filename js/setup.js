04a622775b4f7cd6cca00745ccbe441bb7337fa1
Yuliia.Lukianiuk <lukianiky@gmail.com>
2020-05-19 21:42:06 +0300

    add change eyes-coat-fireball

--- js/setup.js
+++ js/setup.js
@@ -5,13 +5,22 @@ var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
 var similarListElement = document.querySelector('.setup-similar-list');
 var userNameInput = document.querySelector('.setup-user-name');
+var coatChange = document.querySelector('.wizard-coat');
+var eyesChange = document.querySelector('.wizard-eyes');
+var fireballChange = document.querySelector('.setup-fireball-wrap');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector(".setup-similar-item");
 var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var WIZARD_SERNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var ESC_KEYCODE = 27;
 var ENTER_KEYCODE = 13;
+
+var getRandomArrElement = function (arr) {
+  var rand = Math.floor(Math.random() * arr.length);
+  return arr[rand];
+};
 
 setupOpen.addEventListener('click', function() {
   userDialog.classList.remove('hidden');
@@ -67,6 +76,17 @@ userNameInput.addEventListener('invalid', function (evt) {
   }
   });
 
+  coatChange.addEventListener('click', function(evt) {
+    coatChange.style.fill = getRandomArrElement(WIZARD_COAT);
+  });
+
+  eyesChange.addEventListener('click', function() {
+    eyesChange.style.fill = getRandomArrElement(WIZARD_EYES);
+  });
+
+  fireballChange.addEventListener('click', function() {
+    fireballChange.style.backgroundColor = getRandomArrElement(WIZARD_FIREBALL);
+  });
 
 var wizards = [
   {
