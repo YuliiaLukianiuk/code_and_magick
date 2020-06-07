805e33c9de3dbb9bb11a53afcb22db63fa0fcc5c
Yuliia.Lukianiuk <lukianiky@gmail.com>
2020-06-07 21:52:04 +0300

    drap and drop for artifacts

--- js/setup.js
+++ js/setup.js
@@ -24,16 +24,61 @@ userNameInput.addEventListener('invalid', function (evt) {
     userNameInput.setCustomValidity('');
   }
 });
-    // Изменение цвета плаща при клике
-    window.setup = {
-      onWizardCoatClick :  function () {
-        coatChange.style.fill = window.utils.getRandomArrElement(WIZARD_COAT);
-      },
-      onWizardEyesClick: function () {
+window.setup = {
+      // Изменение цвета плаща при клике
+  onWizardCoatClick :  function () {
+    coatChange.style.fill = window.utils.getRandomArrElement(WIZARD_COAT);
+  },
+      // Изменение цвета глаз при клике
+  onWizardEyesClick: function () {
     eyesChange.style.fill = window.utils.getRandomArrElement(WIZARD_EYES);
 },
-onFireballClick: function () {
+        // Изменение цвета фаербола при клике
+
+  onFireballClick: function () {
     fireballChange.style.backgroundColor = window.utils.getRandomArrElement(WIZARD_FIREBALL);
+  },
+  
+        // Перемещение артефакта
+  onShopElementDragstart: function (dragstartEvt) {
+    var artifactsElement = document.querySelector('.setup-artifacts');
+    var draggedItem = null;
+    if (dragstartEvt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = dragstartEvt.target;
+      dragstartEvt.dataTransfer.setData('text/plain', dragstartEvt.target.alt);
+      artifactsElement.style.outline = '2px dashed red';
     }
-  };
+
+    artifactsElement.addEventListener('dragstart', function () {
+      artifactsElement.style.outline = '2px dashed red';
+    });
+
+    artifactsElement.addEventListener('dragover', function (evt) {
+      evt.preventDefault();
+      return false;
+    });
+
+    artifactsElement.addEventListener('drop', function (evt) {
+      evt.preventDefault();
+      evt.target.style.backgroundColor = '';
+      evt.target.appendChild(draggedItem);
+      artifactsElement.style.outline = '';
+    });
+
+    artifactsElement.addEventListener('dragenter', function (evt) {
+      evt.preventDefault();
+      evt.target.style.backgroundColor = 'green';
+    });
+
+    artifactsElement.addEventListener('dragleave', function (evt) {
+      evt.preventDefault();
+      evt.target.style.backgroundColor = '';
+    });
+
+    document.addEventListener('dragend', function (evt) {
+      evt.preventDefault();
+      artifactsElement.style.outline = '';
+    });
+  }
+};
 })();
