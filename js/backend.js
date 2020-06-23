98690ddc16d82a9e92f071e7e2c96b16c849425f
YuliiaLukianiuk <lukianiky@gmail.com>
2020-06-23 21:07:10 +0300

    load,send data on server

--- js/backend.js
+++ js/backend.js
@@ -1,6 +1,7 @@
 'use strict';
 (function () {
-  var URL = 'https://js.dump.academy/code-and-magick';
+  var URL = 'https://javascript.pages.academy/code-and-magick';
+  //объект с данными, которые необходимо отправить, фун-ю обратного вызова, которая будет вызываться, когда данные отправленны успешно
   window.upload = function(data, onSuccess) {
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
--- js/simmilar-wizard.js
+++ js/simmilar-wizard.js
@@ -1,49 +1,43 @@
 'use strict';
 (function() {
   var similarListElement = document.querySelector('.setup-similar-list');
-  
-  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector(".setup-similar-item");
-  var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var WIZARD_SERNAME = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
-  var WIZARDS_COUNT = 4;
-
-  var generateWizards = function (wizardsCount) {
-    var wizardsArray = [];
-    for (var i = 0; i < wizardsCount; i++) {
-      wizardsArray[i] = {
-        name: window.utils.getRandomArrElement(WIZARD_NAMES) + ' ' + window.utils.getRandomArrElement(WIZARD_SERNAME),
-        coatColor: window.utils.getRandomArrElement(WIZARD_COAT),
-        eyesColor: window.utils.getRandomArrElement(WIZARD_EYES)
-      };
-    }
-    return wizardsArray;
-  };
-  
-  var WIZARD_COAT = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector(".setup-similar-item");  
   
   var renderWizard  = function (wizard) {
     var wizardElement = similarWizardTemplate.cloneNode(true);
     
     wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.colorCoat;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.colorEyes;
     return wizardElement;
-  };
-  var renderWizards = function (wizards) {
-    var fragment = document.createDocumentFragment();
-    for (var i = 0; i < wizards.length; i++) {
-      fragment.appendChild(renderWizard(wizards[i]));
-    }
-    return fragment;
-  };
-  
-  var wizards = generateWizards(WIZARDS_COUNT);
-  similarListElement.append(renderWizards(wizards));
-  
-  // Показываем Список похожих персонажей
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  }
+
+  var userDialog = document.querySelector('.setup');
+  var form = userDialog.querySelector('.setup-wizard-form');
+  form.addEventListener('submit', function (evt) {
+    window.upload(new FormData(form), function (response) {
+      userDialog.classList.add('hidden');
+    })
+  });
+  var successHandler = function (wizards) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < 4; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
+  similarListElement.appendChild(fragment);
+  userDialog.querySelector('.setup-similar').classList.remove('hidden');
+};
+var errorHandler = function (errorMessage) {
+  var node = document.createElement('div');
+  node.style = 'z-index: 100; margin:0 auto; text-align: center; background-color: red;';
+  node.style.position = 'absolute';
+  node.style.left = 0;
+  node.style.right = 0;
+  node.style.fontSize = '30px';
+
+  node.textContent = errorMessage;
+  document.body.insertAdjacentElement('afterbegin', node);
+};
+window.load(successHandler, errorHandler);
   
 })();
